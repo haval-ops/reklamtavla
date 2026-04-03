901c858f1096cc5154be291b6782e29b43b3ba9c
haval-ops <haval@multiversenv.com>
2026-04-03 02:45:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,32 @@
 #include "display.h"
 
 int main() {
+    // Initiera slump
     srand(time(NULL));
 
     int lastCustomer = -1;
     int elapsedSeconds = 0;
 
     while (true) {
+        // Räkna minuter sedan start
         int minutesSinceStart = elapsedSeconds / 60;
 
+        // Välj nästa kund (inte samma som förra)
         int customer = pickNextCustomer(lastCustomer);
+
+        // Hämta rätt reklam
         AdMessage ad = getMessageForCustomer(customer, minutesSinceStart);
 
-        std::cout << "------------------------" << std::endl;
-        std::cout << "Ny reklam vald:" << std::endl;
-        std::cout << ad.text << std::endl;
-        std::cout << "Visningstid: 20 sekunder" << std::endl;
+        // Hämta kundnamn
+        std::string customerName = getCustomerName(customer);
 
-        showAd(ad, 20);
+        // Visa reklamen (20 sekunder)
+        showAd(ad, customerName, 20);
 
+        // Uppdatera senaste kund
         lastCustomer = customer;
+
+        // Uppdatera tid
         elapsedSeconds += 20;
     }
 
