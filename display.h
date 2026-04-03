0bcb8f8bb6d84c5dc23a6792b6353d1ff1572fdf
haval-ops <haval@multiversenv.com>
2026-04-03 02:43:33 +0200

    Update display.h

--- display.h
+++ display.h
@@ -2,7 +2,8 @@
 #define DISPLAY_H
 
 #include "ads.h"
+#include <string>
 
-void showAd(const AdMessage& ad, int durationSeconds);
+void showAd(const AdMessage& ad, const std::string& customerName, int durationSeconds);
 
 #endif
