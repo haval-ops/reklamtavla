b93a46cd2cc88f3f599494ce36515572ecb2ac6e
haval-ops <haval@multiversenv.com>
2026-04-03 02:38:55 +0200

    Update ads.h

--- ads.h
+++ ads.h
@@ -1,1 +1,26 @@
+#ifndef ADS_H
+#define ADS_H
 
+#include <string>
+
+enum DisplayMode {
+    STATIC_TEXT,
+    SCROLL_TEXT,
+    BLINK_TEXT
+};
+
+struct AdMessage {
+    std::string text;
+    DisplayMode mode;
+};
+
+struct Customer {
+    std::string name;
+    int weight;
+};
+
+// Funktioner
+int pickNextCustomer(int lastCustomer);
+AdMessage getMessageForCustomer(int customerIndex, int minutesSinceStart);
+
+#endif
