5cd1fe0d4239bc804b1a24a520e6c8ec8a728184
haval-ops <haval@multiversenv.com>
2026-04-03 02:40:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <unistd.h> // sleep
+#include <ctime>
+#include "ads.h"
 
+int main() {
+    srand(time(NULL));
+
+    int lastCustomer = -1;
+    int elapsedSeconds = 0;
+
+    while (true) {
+
+        int minutesSinceStart = elapsedSeconds / 60;
+
+        int customer = pickNextCustomer(lastCustomer);
+
+        AdMessage ad = getMessageForCustomer(customer, minutesSinceStart);
+
+        std::cout << "------------------------" << std::endl;
+        std::cout << "Visar reklam: " << std::endl;
+        std::cout << ad.text << std::endl;
+        std::cout << "Mode: " << ad.mode << std::endl;
+
+        lastCustomer = customer;
+
+        sleep(20); // 20 sekunder
+        elapsedSeconds += 20;
+    }
+
+    return 0;
+}
